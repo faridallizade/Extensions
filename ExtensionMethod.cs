1c2c6a13a167ef06efface0dedba36d096de33a9
faridallizade <alizadebuzinez@gmail.com>
2023-10-18 13:26:35 +0400

    Proje dosyası ekle.

---
+++ ExtensionMethod.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week05Task
+{
+    public static class ExtensionMethod
+    {
+
+
+        public static bool CustomContains(this string exampleWord)
+        {
+            if (exampleWord.Length != 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool CustomContains(this char exampleWord)
+        {
+            if (exampleWord.ToString().Length == 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool IsPrime(this int Digit)
+        {
+            int count = 0;
+            for (int i = 1; i <= Digit; i++)
+            {
+                if (Digit % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count > 2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool IsPowOfTwo(this int Power)
+        {
+            bool Result = true;
+            int Multipler = 2;
+            while (Multipler <= Power)
+            {
+                if (Multipler == Power)
+                {
+                    Result = true;
+                    break;
+                }
+                else
+                {
+                    Result = false;
+                }
+
+                Multipler *= 2;
+            }
+            return Result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Channels;
+
+namespace Week05Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter what you want : ");
+            //var example = Console.ReadLine();
+            //bool check = example.CustomContains();
+            //Console.WriteLine(check);
+
+            //Console.Write("Enter the number what you want to check : ");
+            //int number = Convert.ToInt32(Console.ReadLine());    
+            //bool checkPrime = number.IsPrime();
+            //Console.WriteLine(checkPrime);
+
+            Console.Write("Enter the number to check : ");
+            int powerNumber = Convert.ToInt32(Console.ReadLine());
+            bool CheckPower = powerNumber.IsPowOfTwo();
+            Console.WriteLine(CheckPower);
+        }
+    }
+}
